a772369e64fb1cc8417bb2361759f42a06b08c07
sujayp45 <sujaykumarpandey20@gmail.com>
2026-07-07 11:10:05 +0530

    Assignment1

---
+++ src/datatypes/Assign1Q10.java
@@ -0,0 +1,17 @@
+package datatypes;
+
+public class Assign1Q10 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		double d = 120.20;
+		double a = 135.60;
+
+		System.out.println("sum of number: " + (d + a));
+		System.out.println("diff of number: " + (d - a));
+		System.out.println("multi of number: " + (d * a));
+		System.out.println("divi of number: " + (d / a));
+	}
+
+}
---
+++ src/datatypes/Assign1Q11.java
@@ -0,0 +1,18 @@
+package datatypes;
+
+public class Assign1Q11 {
+
+	int length = 50;
+	int width = 40;
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Assign1Q11 a = new Assign1Q11();
+
+		int result = a.length * a.width;
+
+		System.out.println("Area of rectangle : " + result);
+
+	}
+
+}
